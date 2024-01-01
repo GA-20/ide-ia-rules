2e44d3e90d9cc2d0195942702aabe987168b6207
JC <juliocesarflores12@gmail.com>
2024-01-01 06:37:15 -0400

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@tanstack/react-query";
+import data from "./data/data.json";
+
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+}
+
+const request = async (): Promise<User[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(data as User[]), 2000);
+  });
+};
+
+const useData = () => {
+  const query = useQuery({
+    queryKey: ["data"],
+    queryFn: request,
+  });
+
+  const { data, isLoading, isError, refetch } = query;
+  return { data, isLoading, isError, refetch };
+};
 
 function App() {
+  const { data, isLoading, isError } = useData();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User List</h1>
+
+      <ul>
+        {data?.map((user, index) => (
+          <li key={index}>{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
